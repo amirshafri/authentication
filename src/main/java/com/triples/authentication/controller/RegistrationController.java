9f03f10519958e808e08ef475f77a093a978b4a3
amirshafri <amir.shafri@yahoo.com>
2023-09-24 17:47:02 +0800

    center header title and include temp role

--- src/main/java/com/triples/authentication/controller/RegistrationController.java
+++ src/main/java/com/triples/authentication/controller/RegistrationController.java
@@ -2,10 +2,16 @@ package com.triples.authentication.controller;
 
 import com.triples.authentication.dto.UserDto;
 import com.triples.authentication.entities.User;
+import com.triples.authentication.model.BankingRole;
 import com.triples.authentication.model.UserRegistration;
 import com.triples.authentication.services.RegistrationService;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import jdk.nashorn.internal.runtime.logging.Logger;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class RegistrationController {
 
     private final RegistrationService registrationService;
@@ -33,12 +40,13 @@ public class RegistrationController {
     public String showRegistrationForm(Model model) {
         model.addAttribute("userRegistration", new UserRegistration());
         // You can also add roles to the model if needed
-        // model.addAttribute("roles", rolesList);
+        model.addAttribute("roles", Arrays.asList(BankingRole.values()));
         return "registration";
     }
 
     @PostMapping("/register")
     public String processRegistration(@ModelAttribute("userRegistration") UserRegistration userRegistration) {
+        log.info(" userRegistration {}", userRegistration);
         // Handle form submission and registration logic here
         return "login"; // Redirect to a success page
     }
